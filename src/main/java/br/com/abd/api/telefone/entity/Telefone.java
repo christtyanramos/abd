8ddb44a6177783ae529454dcef3d3483a96a798d
christtyanramos <christtyanr@gmail.com>
2020-07-23 09:56:02 -0300

    Mapear entidades.

---
+++ src/main/java/br/com/abd/api/telefone/entity/Telefone.java
@@ -0,0 +1,73 @@
+package br.com.abd.api.telefone.entity;
+
+import br.com.abd.api.usuario.entity.Usuario;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "TB_TELEFONE")
+public class Telefone implements Serializable {
+
+    private static final long serialVersionUID = -2406055461208684857L;
+
+    @Id
+    @GeneratedValue(generator = "SEQ_TB_TELEFONE", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "SEQ_TB_TELEFONE", sequenceName = "SEQ_TB_TELEFONE", allocationSize = 1)
+    @Column(name = "CD_TELEFONE", nullable = false)
+    private Long id;
+
+    @Column(name = "CD_USUARIO", nullable = false)
+    private Long usuario_id;
+
+    @Column(name = "DS_NUMERO", length = 9)
+    private String numero;
+
+    @Column(name = "DS_DDD", length = 2)
+    private String ddd;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CD_USUARIO", nullable = false, insertable = false, updatable = false)
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(Long usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ src/main/java/br/com/abd/api/usuario/entity/Usuario.java
@@ -0,0 +1,107 @@
+package br.com.abd.api.usuario.entity;
+
+import br.com.abd.api.telefone.entity.Telefone;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_USUARIO")
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = -4593497438317795120L;
+
+    @Id
+    @GeneratedValue(generator = "SEQ_TB_USUARIO", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "SEQ_TB_USUARIO", sequenceName = "SEQ_TB_USUARIO", allocationSize = 1)
+    @Column(name = "CD_USUARIO", nullable = false)
+    private Long id;
+
+    @Column(name = "DS_NOME")
+    private String nome;
+
+    @Column(name = "DS_EMAIL")
+    private String email;
+
+    @Column(name = "DS_SENHA")
+    private String senha;
+
+    @Column(name = "DT_CRIACAO")
+    private Date data_criacao;
+
+    @Column(name = "DT_ATUALIZACAO")
+    private Date data_atualizacao;
+
+    @Column(name = "DT_ULTIMO_LOGIN")
+    private Date ultimo_login;
+
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+    private List<Telefone> telefones = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(Date data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+
+    public Date getData_atualizacao() {
+        return data_atualizacao;
+    }
+
+    public void setData_atualizacao(Date data_atualizacao) {
+        this.data_atualizacao = data_atualizacao;
+    }
+
+    public Date getUltimo_login() {
+        return ultimo_login;
+    }
+
+    public void setUltimo_login(Date ultimo_login) {
+        this.ultimo_login = ultimo_login;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+}
